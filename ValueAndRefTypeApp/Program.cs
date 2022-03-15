fb68915ad23e1d1e232f813b84769d8d19d10be3
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-03-15 12:19:03 +0300

    ValueType

--- ValueAndRefTypeApp/Program.cs
+++ ValueAndRefTypeApp/Program.cs
@@ -9,4 +9,14 @@ var p2 = p1;
 p2.X = 3;
 Console.WriteLine($"p1: {p1.X} - {p1.Y}");
 Console.WriteLine($"p2: {p2.X} - {p2.Y}");
+
+var p3 = new ValueAndReferenceTypes.ValueType
+{
+    X = 1,
+    Y = 2
+};
+var p4 = p1;
+p4.X = 3;
+Console.WriteLine($"p3: {p3.X} - {p3.Y}");
+Console.WriteLine($"p4: {p4.X} - {p4.Y}");
 Console.ReadLine();
--- ValueAndRefTypeTests/ValueAndRefTypeTests.cs
+++ ValueAndRefTypeTests/ValueAndRefTypeTests.cs
@@ -21,5 +21,20 @@ namespace ValueAndRefTypeTests
             //Assert
             Assert.Equal(p1.X, p2.X);
         }
+        [Fact]
+        public void ValueTypeTest()
+        {
+            //Arrange
+            var p1 = new ValueAndReferenceTypes.ValueType
+            {
+                X = 1,
+                Y = 2
+            };
+            var p2 = p1;
+            //Act
+            p1.X = 20;
+            //Assert
+            Assert.NotEqual(p1.X, p2.X);
+        }
     }
 }
