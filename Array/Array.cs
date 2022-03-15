7abb688d705847d4515f0c7944f8561557d24f56
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-03-15 18:33:03 +0300

    Array Custom Enumerator

--- Array/Array.cs
+++ Array/Array.cs
@@ -43,30 +43,5 @@ namespace Datastructures.Array
             return new CustomArrayEnumerator(InnerArray);
         }
     }
-
-    public class CustomArrayEnumerator : IEnumerator
-    {
-        public Object[] _array;
-        private int index=-1;
-        public CustomArrayEnumerator(Object[] array)
-        {
-            _array = array;
-        }
-
-        public object Current => _array[index];
-        public bool MoveNext()
-        {
-            if (index<_array.Length-1)
-            {
-                index++;
-                return true;
-            }
-            return false;
-        }
-
-        public void Reset()
-        {
-            index = -1;
-        }
-    }
 }
+
---
+++ Array/CustomArrayEnumerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace Datastructures.Array
+{
+    public class CustomArrayEnumerator : IEnumerator
+    {
+        public Object[] _array;
+        private int index=-1;
+        public CustomArrayEnumerator(Object[] array)
+        {
+            _array = array;
+        }
+
+        public object Current => _array[index];
+        public bool MoveNext()
+        {
+            if (index<_array.Length-1)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
