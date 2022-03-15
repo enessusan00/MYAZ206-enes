03fc9c98968d58b6c3d9041983ec425e8b2f0086
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-03-15 14:22:51 +0300

    Array

---
+++ ArrayApp/Program.cs
@@ -0,0 +1,24 @@
+﻿var arr = Array.CreateInstance(typeof(int), 4);
+arr.SetValue(23, 0);
+arr.SetValue(13, 1);
+arr.SetValue(14, 2);
+arr.SetValue(15, 3);
+
+Console.WriteLine(arr.GetValue(3));
+try
+{
+    Console.WriteLine(arr.GetValue(5));
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+//Console.WriteLine(arr.GetValue(5)); olmayan bi elemanı isteyemeyiz fixed size
+
+
+Console.ReadLine();
